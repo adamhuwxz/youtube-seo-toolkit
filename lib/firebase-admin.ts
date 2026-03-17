366b41f73375cd6e749ca1a5fdf102d1eabf8521
Adam Jones <adamhuwxz@gmail.com>
2026-03-17 00:30:29 +0000

    update

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,4 +1,4 @@
-import { cert, getApps, initializeApp } from "firebase-admin/app";
+import { getApps, initializeApp, cert, applicationDefault } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
 
@@ -6,28 +6,23 @@ const projectId = process.env.FIREBASE_ADMIN_PROJECT_ID;
 const clientEmail = process.env.FIREBASE_ADMIN_CLIENT_EMAIL;
 const privateKey = process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, "\n");
 
-if (!projectId) {
-  throw new Error("Missing FIREBASE_ADMIN_PROJECT_ID");
-}
-
-if (!clientEmail) {
-  throw new Error("Missing FIREBASE_ADMIN_CLIENT_EMAIL");
-}
-
-if (!privateKey) {
-  throw new Error("Missing FIREBASE_ADMIN_PRIVATE_KEY");
-}
-
 const app =
   getApps().length > 0
     ? getApps()[0]
-    : initializeApp({
-        credential: cert({
-          projectId,
-          clientEmail,
-          privateKey,
-        }),
-      });
+    : initializeApp(
+        clientEmail && privateKey
+          ? {
+              credential: cert({
+                projectId,
+                clientEmail,
+                privateKey,
+              }),
+            }
+          : {
+              credential: applicationDefault(),
+              projectId,
+            }
+      );
 
 export const adminAuth = getAuth(app);
 export const adminDb = getFirestore(app);
