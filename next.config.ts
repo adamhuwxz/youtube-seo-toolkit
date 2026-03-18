dc0713e82209b212d64770d7c7a318d9c7d85196
Adam Jones <adamhuwxz@gmail.com>
2026-03-18 20:40:59 +0000

    updated and added new tool

--- next.config.ts
+++ next.config.ts
@@ -40,6 +40,28 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com https://b.stripecdn.com https://newassets.hcaptcha.com",
+              "frame-src 'self' https://js.stripe.com https://hooks.stripe.com https://checkout.stripe.com https://b.stripecdn.com https://newassets.hcaptcha.com",
+              "connect-src 'self' https://api.stripe.com https://r.stripe.com https://b.stripecdn.com https://newassets.hcaptcha.com",
+              "img-src 'self' data: https://*.stripe.com https://*.hcaptcha.com",
+              "style-src 'self' 'unsafe-inline'",
+              "font-src 'self' data:",
+            ].join("; "),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
