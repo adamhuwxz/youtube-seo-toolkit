e4bc1e759853c46ceb62571c89d08477150a9442
Adam Jones <adamhuwxz@gmail.com>
2026-03-20 23:10:47 +0000

    updated and added new tools

--- next.config.ts
+++ next.config.ts
@@ -42,8 +42,28 @@ const nextConfig: NextConfig = {
   },
 
   async headers() {
-  return [];
-}
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com https://b.stripecdn.com https://newassets.hcaptcha.com https://accounts.google.com https://apis.google.com",
+              // Added firebaseapp.com to frame-src for popups/iframes
+              "frame-src 'self' https://js.stripe.com https://hooks.stripe.com https://checkout.stripe.com https://b.stripecdn.com https://newassets.hcaptcha.com https://accounts.google.com https://*.firebaseapp.com",
+              // Added firebaseapp.com to connect-src for auth polling/requests
+              "connect-src 'self' https://api.stripe.com https://r.stripe.com https://b.stripecdn.com https://newassets.hcaptcha.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://firebaseinstallations.googleapis.com https://firestore.googleapis.com https://www.googleapis.com https://*.googleapis.com https://*.firebaseio.com wss://*.firebaseio.com https://*.firebaseapp.com",
+              "img-src 'self' data: https://*.stripe.com https://*.hcaptcha.com https://*.googleusercontent.com",
+              "style-src 'self' 'unsafe-inline'",
+              "font-src 'self' data:",
+            ].join("; "),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
